c8fdd57fcb9cad1f7d66b64e73ad2246ff1578a0
Vincent Bonamy <vbonamy@gmail.com>
2026-03-28 09:19:54 +0100

    Dev docker env : use esup-otp-api 2.0.2 has_enabled_method attribute in groovy script

--- src/etc/docker/cas-config/config/mfaGroovyTrigger.groovy
+++ src/etc/docker/cas-config/config/mfaGroovyTrigger.groovy
@@ -7,7 +7,7 @@ class SampleGroovyEventResolver {
         def esupOtpService = ctx.getBean("esupOtpService")
         def esupOtpUserInfos = esupOtpService.getUserInfos(authentication.principal.id)
         def esupOtpMethods = esupOtpUserInfos.get("user").get("methods").toMap().values()
-	    def oneMfaMethodIsActive = esupOtpMethods.stream().anyMatch(method -> method instanceof Map && method.get("active"))
+	def oneMfaMethodIsActive = esupOtpUserInfos.get("user").getBoolean("has_enabled_method")
         logger.warn("User ${authentication.principal.id} has the following MFA methods: ${esupOtpMethods}")
         logger.warn(oneMfaMethodIsActive.toString())
         return oneMfaMethodIsActive ? "mfa-esupotp" : null
